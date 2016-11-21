628089061595d1e2d9345f4f331652df0d726059
Greg Korthuis <gpkort@gmail.com>
2016-11-20 17:18:33 -0700

    Initial package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='statscrapper',
+      version='0.1',
+      description='Package to download NFL stats',
+      url='http://github.com/gpkort/statscraper',
+      author='Greg Korthuis',
+      author_email='gpkort@gmail.com',
+      license='Apache 2.0 License',
+      packages=['statscrapper'],
+      zip_safe=False)
