b4a3ae0af2543640dd2c81ee11daf7fbb0850278
Greg Korthuis <gpkort@gmail.com>
2017-01-23 19:18:15 -0700

    unit tests

--- prostatscrapper/prostatscrapper.py
+++ prostatscrapper/prostatscrapper.py
@@ -1,29 +1,33 @@
 from enum import Enum
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
 
 class POSITION(Enum):
     NONE = 0,
     RUNNINBACK = 1
 
-STANDARD_URL = 'http://www.pro-football-reference.com/players'
+
+STANDARD_URL = 'http://www.pro-football-reference.com'
+PLAYERS = '/players'
 RUNNINGBACK = 'rb'
 SLASH = '/'
-LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-           't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-class statscrapper(object):
-    __PLAYER_DICTIONARY = {POSITION.RUNNINBACK: 'rb'}
+class ProStatScrapper(object):
+    __PLAYER_DICTIONARY = {POSITION.RUNNINBACK: 'RB'}
 
     __rb_dataset = None
 
     def __init__(self, path=None):
-        self.url =  STANDARD_URL if path is None else path
+        self.url = STANDARD_URL if path is None else path
 
     def get_all(self):
         pass
 
     def get_all_by_position(self, position):
-        pass
+        return None
 
     def get_all_by_last(self, letter):
         pass
@@ -31,7 +35,42 @@ class statscrapper(object):
     def get_by_name(self, name, position=POSITION.NONE):
         pass
 
+    def get_all_columns(self):
+        column_dict = {}
+
+        for pos in POSITION:
+            if pos == POSITION.NONE:
+                continue
+            else:
+                column_dict[pos] = self.get_columns(pos)
+
+            return column_dict
+
+    def get_columns(self, position):
+        if position == POSITION.RUNNINBACK:
+            return ['id', 'name', 'year', 'g', 'gs', 'rush_att', 'rush_yds', 'rush_td', 'rush_long',
+                    'rush_yds_per_att', 'rush_yds_per_g', 'rush_att_per_g', 'rec', 'rec_yds',
+                    'rec_yds_per_rec', 'rec_td', 'rec_long', 'rec_per_g', 'rec_yds_per_g',
+                    'yds_from_scrimmage', 'rush_receive_td', 'fumbles']
+        else:
+            return None
+
+    def get_players_dataset(self, position, letter):
+        req = requests.get("http://www.pro-football-reference.com" + '/' + PLAYERS + '/' + letter)
+        soup = BeautifulSoup(req.text, "lxml")
+        players = soup.find_all('div', {'class': 'section_content'})
+        player_df = pd.DataFrame(columns=('link', 'name'))
+        i = 0
+        pos = '(' + ProStatScrapper.__PLAYER_DICTIONARY[position] + ')'
+
+        for div in players:
+            for line in div.find_all('p'):
+                if pos in line.text:
+                    link = line.find('a')
+                    player_df.loc[i] = [str(link['href']), link.text]
+                    i += 1
+
+        return player_df
+
     def __str__(self):
-        return str.format('Pro footbal stats {}', self.url)
-
-
+        return str.format('Pro football stats {}', self.url)
--- tests/prostatscrapperTests.py
+++ tests/prostatscrapperTests.py
@@ -1,4 +1,27 @@
 import unittest
 import prostatscrapper
+from prostatscrapper.prostatscrapper import ProStatScrapper, POSITION
 
-class prostatscrapper(unittest.te)
+
+class prostatscrapperTests(unittest.TestCase):
+    def setUp(self):
+        self.scrapper = ProStatScrapper()
+
+    def test_get_columns(self):
+        columns = self.scrapper.get_columns(POSITION.RUNNINBACK)
+        self.assertIsNotNone(columns, 'Columns are none')
+        self.assertEqual(22, len(columns), 'RB column length incorrect')
+
+        columns = self.scrapper.get_columns(POSITION.NONE)
+        self.assertIsNone(columns, 'Columns are not none')
+
+    def test_get_all_columns(self):
+        col_dict = self.scrapper.get_all_columns()
+        self.assertIsNotNone(col_dict)
+        self.assertEqual(len(POSITION)-1, len(col_dict), 'Dictionary length incorrect')
+
+    def test_get_position(self):
+        players_dict = self.scrapper.get_all_by_position(POSITION.RUNNINBACK)
+        self.assertIsNone(players_dict, 'All positions are None')
+
+
